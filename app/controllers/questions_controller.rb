4f37d0eb6f111c662a2a5bcaa5ba275f4a233743
poloz0ter <polozoter@gmail.com>
2019-02-06 08:04:16 +1100

    corrections

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :find_test_questions
+  before_action :find_question
   rescue_from NoMethodError, with: :rescue_with_question_not_found
+  rescue_from RuntimeError, with: :rescue_with_question_body_is_empty
 
   def index
     render inline: "
@@ -11,7 +13,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = @questions[params[:id].to_i - 1]
+    @question
   end
 
   def new
@@ -19,14 +21,11 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(question_body)
-    @question.test_id = params[:test_id]
-    @question.save
-    redirect_to test_questions_path
+    @test = Test.find(params[:test_id])
+    @test.questions.create(question_body)
   end
 
   def destroy
-    show
     @question.destroy!
     redirect_to test_questions_path
   end
@@ -37,11 +36,19 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:body)
   end
 
+  def find_question
+    @question = Question.find_by id: params[:id], test_id: params[:test_id]
+  end
+
   def find_test_questions
     @questions = Test.find(params[:test_id]).questions
   end
 
   def rescue_with_question_not_found
     render plain: "Question not found"
+  end
+
+  def rescue_with_question_body_is_empty
+    render plain: "Not Created. Question can't be empty."
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ApplicationRecord
+  before_validation :validates_body
   belongs_to :test
   has_many :answers
+
+  def validates_body
+    raise if body.empty?
+  end
 end
