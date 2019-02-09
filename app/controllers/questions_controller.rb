2b61bfd6b3fd12403cedfbf3bb48a576f3e96800
poloz0ter <polozoter@gmail.com>
2019-02-09 14:37:14 +1100

    Tests with navigation, footer & actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,37 +1,50 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create]
-  before_action :find_question, only: %i[show destroy]
-  rescue_from NoMethodError, with: :rescue_with_question_not_found
-  rescue_from RuntimeError, with: :rescue_with_question_body_is_empty
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy edit update]
+  #rescue_from NoMethodError, with: :rescue_with_question_not_found
+  #rescue_from RuntimeError, with: :rescue_with_question_body_is_empty
 
   def index
-    render inline: "
-    <p><%= 'EMPTY TEST' if @test.questions.empty? %></p>
-    <% @test.questions.each do |question| %>
-      <br><%= question.body %></br>
-    <% end %>"
+    @id = @test.id
+    @number_of_tests = Test.count
+    if @number_of_tests > 0
+      @previous_test = Test.find(@id - 1) if @id > 1 
+      @next_test = Test.find(@id + 1) if @id < @number_of_tests
+    else 
+      render plain: 'Empty'
+    end
   end
 
   def show
-    @question
+    
   end
 
   def new
-
+    @question = @test.questions.new
   end
 
   def create
-    question = @test.questions.new(question_body)
-    if question.save
+    @question = @test.questions.new(question_body)
+    if @question.save
       redirect_to test_questions_path
     else
-      render plain: "Not saved"
+      render :new
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @question.update(question_body)
+    redirect_to test_questions_path(@question.test)
   end
 
   def destroy
     @question.destroy
-    render inline: "<p>Question deleted.</p>"
+    render inline: "<p>Question deleted</p>
+                    <%= render 'back_button' %>"
   end
 
   private
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,20 @@
 module QuestionsHelper
+  def question_header(action)
+    case action
+    when 'new'
+      "Create New #{@test.title} Question"
+    when 'edit'
+      "Edit #{@question.test.title} Question"
+    else
+      "Testguru"
+    end
+  end
+
+  def year_now
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
 end
