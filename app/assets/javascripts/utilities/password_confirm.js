222fc0d68443df1b38d6cd261aab1c6f5f66d86b
poloz0ter <polozoter@gmail.com>
2019-04-04 17:48:43 +1100

    corrections

--- app/assets/javascripts/utilities/password_confirm.js
+++ app/assets/javascripts/utilities/password_confirm.js
@@ -1,7 +1,9 @@
 document.addEventListener('turbolinks:load', function() {
 	var confirmation = document.querySelector('.password_confirmation')
+	var password = document.querySelector('.password')
 
 	if (confirmation) { confirmation.addEventListener('input', checkConfirmation) }
+	if (confirmation) { password.addEventListener('input', checkConfirmation) }
 })
 
 function checkConfirmation() {
@@ -11,11 +13,14 @@ function checkConfirmation() {
 	var check = document.querySelector('.octicon-check')
 	var x = document.querySelector('.octicon-x')
 
+	if (confirmation.value == "") {
+		check.classList.add('hide')
+		x.classList.add('hide')
+		return
+	}
+
 	if (confirmation.value == password.value) {
 		check.classList.remove('hide')
-		x.classList.add('hide')
-	} else if (confirmation.value == "") {
-		check.classList.add('hide')
 		x.classList.add('hide')
 	} else {
 		check.classList.add('hide')
