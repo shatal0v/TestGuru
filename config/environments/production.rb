9f7e59f0ec6418898fdac75b35694753e04ce4a2
poloz0ter <polozoter@gmail.com>
2019-04-09 15:06:56 +1100

    2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ Rails.application.configure do
 
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'damp-eyrie-21953.herokuapp.com' }
   config.action_mailer.delivery_method =:smtp
   config.action_mailer.smtp = {
     address:                'smtp.gmail.com',
