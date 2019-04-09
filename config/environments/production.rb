1d00f55d39e0d74cf0d4ec7fae9b2da3a892aa87
poloz0ter <polozoter@gmail.com>
2019-04-09 15:33:06 +1100

    action_mailer conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "testguru_#{Rails.env}"
 
-
-
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
