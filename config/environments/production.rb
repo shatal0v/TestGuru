bdd3204d150e071f049ec2b0ada2957260641d75
poloz0ter <polozoter@gmail.com>
2019-04-09 14:40:48 +1100

    2'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'damp-eyrie-21953.herokuapp.com' }
   config.action_mailer.delivery_method =:smtp
   config.action_mailer.smtp = {
-    address:                'smtp/.gmail.com'
+    address:                'smtp.gmail.com'
     port:                   587,
     user_name:              ENV['SMTP_USERNAME'],
     password:               ENV['SMTP_PASSWORD'],
