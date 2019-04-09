698f27562816a5c5ee6d467ae0e99e8101f34e41
poloz0ter <polozoter@gmail.com>
2019-04-09 14:33:57 +1100

    action_mailer config2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,12 +70,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'damp-eyrie-21953.herokuapp.com' }
   config.action_mailer.delivery_method =:smtp
   config.action_mailer.smtp = {
-    address:              'smtp/.gmail.com'
-    port:                 587,
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    address:                'smtp/.gmail.com'
+    port:                   587,
+    user_name:              ENV['SMTP_USERNAME'],
+    password:               ENV['SMTP_PASSWORD'],
+    authentication:         'plain',
+    enable_starttls_auto:   true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
