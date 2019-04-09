5ca1071d44023e86d19c2a883b3d6be8c8c49388
poloz0ter <polozoter@gmail.com>
2019-04-09 15:40:45 +1100

    action_mailer conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "testguru_#{Rails.env}"
 
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'radiant-ocean-94935.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
