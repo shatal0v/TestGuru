ae04eee74c88d352f76f3658b2c7a01075e592c0
poloz0ter <polozoter@gmail.com>
2019-04-10 12:45:24 +1100

    empty tests resque

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -4,8 +4,8 @@ class Admin::QuestionsController < Admin::BaseController
   before_action :authenticate_user!
   before_action :find_test, only: %i[index create new]
   before_action :find_question, only: %i[show destroy edit update]
-  # rescue_from NoMethodError, with: :rescue_with_question_not_found
-  # rescue_from RuntimeError, with: :rescue_with_question_body_is_empty
+  rescue_from NoMethodError, with: :rescue_with_question_not_found
+  rescue_from RuntimeError, with: :rescue_with_question_body_is_empty
 
   def show; end
 
